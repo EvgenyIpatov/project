1e1d862425fea46d2ebba6513a19f2ef322f514a
Ivan <exemple@gmail.com>
2020-08-25 19:06:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+//'use strict';
+
+a = 5;
+console.log(a);
+
+
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+//console.log(4/0);
+console.log('sring'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.pnj','orange.jpg','apple.bmp'];
+
+console.log(arr[0])
+
+//alert("Helo World");
+
+//let answer = +prompt("Есть ли вам 18" );
+
+//console.log(answer);
+
+
+//console.log("arr" + "- object")
+//console.log(4 + +"- object");
+
+
+
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+
+
+
+
+
